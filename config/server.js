4f89f3f078b117cdda2e62111524f545538ab34f
niyaaz_alpha <niyaaz79red@gmail.com>
2020-11-11 22:28:21 +0200

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e5ebbe41e7013759fa043ea549d0b660'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '1bb266aa-1d9c-4bb0-92ec-b815c7499a06'
+};
